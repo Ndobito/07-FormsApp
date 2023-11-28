b4a655a0f5eb92365e25835b999ecb5556daf80d
Ndobito <jcuellarmenza@gmail.com>
2023-11-28 11:31:07 -0500

    Advance part 18

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import * as customValidators from 'src/app/shared/validators/validators';
+import { ValidatorsService } from '../../service/validators.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -9,19 +11,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterPageComponent {
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
+    username: ['', [Validators.required, this.validatorsService.canBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]]
   })
 
   constructor(
-    private fb:FormBuilder
+    private fb:FormBuilder, 
+    private validatorsService: ValidatorsService, 
   ){}
 
-  isValied(field: string){
-    //TODO: Obtener validacion desde un servicio
+  isValiedField(field: string){
+    return this.validatorsService.isValidField(this.myForm, field)
   }
 
   onSubmit():void{
